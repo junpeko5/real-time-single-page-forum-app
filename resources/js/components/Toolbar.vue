0488ff6af48680f60186b4dede1fe076382b4f3f
junpeko5 <junpeko5@gmail.com>
2019-01-25 01:02:32 +0900

    Notifications FrontEnd

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -3,6 +3,7 @@
         <!--<v-toolbar-side-icon></v-toolbar-side-icon>-->
         <v-toolbar-title>Bitfumes</v-toolbar-title>
         <v-spacer></v-spacer>
+        <app-notification v-if="loggedIn"></app-notification>
         <div class="hidden-sm-and-down">
             <router-link
                 v-for="item in items"
@@ -18,9 +19,14 @@
 </template>
 
 <script>
+    import AppNotification from './AppNotification';
     export default {
+        components: {
+            AppNotification
+        },
         data() {
             return {
+                loggedIn: User.loggedIn(),
                 items: [
                     {
                         title: 'Forum',
