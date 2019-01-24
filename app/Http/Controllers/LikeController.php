daaea04f8b48569d6f1459968aafe7753bfb4d80
junpeko5 <junpeko5@gmail.com>
2019-01-24 22:59:07 +0900

    Likeコンポーネントの作成

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -23,8 +23,7 @@ class LikeController extends Controller
     public function likeIt(Reply $reply)
     {
         $reply->like()->create([
-//            'user_id' => auth()->id()
-            'user_id' => '1'
+            'user_id' => auth()->id()
         ]);
     }
 
@@ -34,7 +33,6 @@ class LikeController extends Controller
      */
     public function unLikeIt(Reply $reply)
     {
-//        $reply->like()->where(['user_id', auth()->id()])->first()->delete();
-        $reply->like()->where('user_id', 1)->first()->delete();
+        $reply->like()->where('user_id', auth()->id())->first()->delete();
     }
 }
--- resources/js/components/reply/Reply.vue
+++ resources/js/components/reply/Reply.vue
@@ -4,6 +4,8 @@
             <v-card-title>
                 <div class="headline">{{data.user}}</div>
                 <div class="ml-2">said {{data.created_at}}</div>
+                <v-spacer></v-spacer>
+                <like :content="data"></like>
             </v-card-title>
             <v-divider></v-divider>
             <edit-reply
@@ -31,13 +33,15 @@
 
 <script>
     import EditReply from './EditReply'
+    import Like from '../likes/Like'
     export default {
         props: [
             'data',
             'index'
         ],
         components: {
-            EditReply
+            EditReply,
+            Like
         },
         data() {
             return {
